dc029b5c1a5392a06e9844546714432a3e2dd041
Lorena <lorena.giddens@outlook.com>
2018-04-21 08:00:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int % 3 && 5 == 0
     "FizzBuzz"
     elsif int % 5 == 0
       "Buzz"
-    elsif int % 3 && 5 == 0
+    elsif int % 3 == 0
       "FizzBuzz"
       else !(int % 3 || 5 == 0)
         nil
